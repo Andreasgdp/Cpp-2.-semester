560c056433bd16818141ec9b86618ef8438e283b
Andreas <andreasgdp@gmail.com>
2021-02-07 14:19:52 +0100

    Working QuadraticEquation

--- Andreas/Lektion 1/VS code/QuadraticEquation/Headers/QuadraticEquation.h
+++ Andreas/Lektion 1/VS code/QuadraticEquation/Headers/QuadraticEquation.h
@@ -1,11 +1,18 @@
-#pragma once
+#ifndef QUADRATICEQUATION_H
+#define QUADRATICEQUATION_H
+#include <tuple>
+
+
 class QuadraticEquation
 {
-
 public:
-	void solve();
+    QuadraticEquation();
+    QuadraticEquation(double a, double b, double c);
+
+    std::tuple<double, double, bool> solve() const;
+
 private:
-	double mA;
-	double mB;
-	double mC;
+    double mA, mB, mC;
 };
+
+#endif // QUADRATICEQUATION_H
--- Andreas/Lektion 1/VS code/QuadraticEquation/scr/QuadraticEquation.cpp
+++ Andreas/Lektion 1/VS code/QuadraticEquation/scr/QuadraticEquation.cpp
@@ -1,1 +1,34 @@
 #include "../Headers/QuadraticEquation.h"
+#include <math.h>
+#include <tuple>
+#include <complex>
+#include <iostream>
+
+QuadraticEquation::QuadraticEquation()
+{
+}
+
+QuadraticEquation::QuadraticEquation(double a, double b, double c)
+{
+	mA = a;
+	mB = b;
+	mC = c;
+}
+
+std::tuple<double, double, bool> QuadraticEquation::solve() const
+{
+	// Negative determinant, imaginary
+	if (double d = pow(mB, 2) - 4 * mA * mC < 0)
+	{
+		// Real number
+		std::complex<double> imag(-mB / (2 * mA), sqrt(abs(pow(mB, 2) - 4 * mA * mC)) / (2 * mA));
+
+		return {std::real(imag), std::imag(imag), true};
+	}
+	else
+	{ // Positive determinant
+		double x1 = (-mB + sqrt(pow(mB, 2) - 4 * mA * mC)) / (2 * mA);
+		double x2 = (-mB - sqrt(pow(mB, 2) - 4 * mA * mC)) / (2 * mA);
+		return {x1, x2, false};
+	}
+}
