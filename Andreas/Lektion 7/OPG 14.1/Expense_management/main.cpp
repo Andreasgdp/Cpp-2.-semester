c105e03e65062c4426ca432d9bc2d2dfecdefee5
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2021-03-15 11:25:31 +0100

    Added products and categories to vector

--- Andreas/Lektion 7/OPG 14.1/Expense_management/main.cpp
+++ Andreas/Lektion 7/OPG 14.1/Expense_management/main.cpp
@@ -1,22 +1,78 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <vector>
 
 using namespace std;
 
+int checkForFail();
+
 int main()
 {
+    std::ifstream ifs;
+    std::string triggerWords("../triggerWords.txt");
+    ifs.open(triggerWords);
+    checkForFail();
+    std::vector<std::vector<std::string>> productCategory;
+    while (!ifs.eof())
+    {
+        std::string line;
+        std::getline(ifs, line);
+        std::istringstream sline{line};
+
+        // Skip empty lines
+        if (line.size() == 0)
+        {
+            continue;
+        }
+
+        std::string product;
+        std::getline(sline, product, ';');
+        std::cout << std::setw(20) << std::left << product;
+
+        std::string category;
+        std::getline(sline, category, ';');
+        std::cout << std::setw(20) << std::left << category << std::endl;
+
+        std::vector<std::string> tempVec{product, category};
+        productCategory.push_back(tempVec);
+    }
+
+    
+    ifs.close();
+
     std::string receiptName;
     cout << "Enter receipt name: ";
     std::cin >> receiptName;
 
-    std::ifstream ifs;
     std::string receipt("../" + receiptName + ".txt");
     ifs.open(receipt);
+    checkForFail();
 
-    if (ifs.fail()) {
+    /*
+    while (!ifs.eof()) {
+
+
+        if (ifs.fail()) {
+            ifs.clear(ifs.rdstate() & ~std::ios_base::failbit);
+        }
+    }
+    */
+
+    ifs.close();
+
+    return 0;
+}
+
+int checkForFail()
+{
+    std::ifstream ifs;
+    if (ifs.fail())
+    {
         std::cerr << "Failed to open receipt. Closing program." << std::endl;
         return 1;
     }
-
-    ifs.close();
+    return 0;
 }
