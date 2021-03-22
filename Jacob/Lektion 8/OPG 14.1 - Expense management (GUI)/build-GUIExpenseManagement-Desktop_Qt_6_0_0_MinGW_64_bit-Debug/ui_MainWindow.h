956c33b3b21aff9c3b719630c3f32ddcc7c50d72
JBLarsen2107 <45763482+JBLarsen2107@users.noreply.github.com>
2021-03-22 09:51:50 +0100

    GUI Expens

---
+++ Jacob/Lektion 8/OPG 14.1 - Expense management (GUI)/build-GUIExpenseManagement-Desktop_Qt_6_0_0_MinGW_64_bit-Debug/ui_MainWindow.h
@@ -0,0 +1,151 @@
+/********************************************************************************
+** Form generated from reading UI file 'MainWindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.0.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QVBoxLayout *verticalLayout;
+    QWidget *widget_2;
+    QHBoxLayout *horizontalLayout;
+    QWidget *widget_3;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label;
+    QWidget *widget_5;
+    QVBoxLayout *verticalLayout_3;
+    QLineEdit *leReceipt;
+    QWidget *widget_4;
+    QVBoxLayout *verticalLayout_2;
+    QPushButton *btnShow;
+    QWidget *widget;
+    QHBoxLayout *horizontalLayout_3;
+    QPlainTextEdit *pteOutput;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayout = new QVBoxLayout(centralwidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        widget_2 = new QWidget(centralwidget);
+        widget_2->setObjectName(QString::fromUtf8("widget_2"));
+        horizontalLayout = new QHBoxLayout(widget_2);
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        widget_3 = new QWidget(widget_2);
+        widget_3->setObjectName(QString::fromUtf8("widget_3"));
+        horizontalLayout_2 = new QHBoxLayout(widget_3);
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        horizontalLayout_2->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(widget_3);
+        label->setObjectName(QString::fromUtf8("label"));
+
+        horizontalLayout_2->addWidget(label);
+
+
+        horizontalLayout->addWidget(widget_3);
+
+        widget_5 = new QWidget(widget_2);
+        widget_5->setObjectName(QString::fromUtf8("widget_5"));
+        verticalLayout_3 = new QVBoxLayout(widget_5);
+        verticalLayout_3->setSpacing(0);
+        verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
+        verticalLayout_3->setSizeConstraint(QLayout::SetDefaultConstraint);
+        verticalLayout_3->setContentsMargins(0, 0, 0, 0);
+        leReceipt = new QLineEdit(widget_5);
+        leReceipt->setObjectName(QString::fromUtf8("leReceipt"));
+
+        verticalLayout_3->addWidget(leReceipt);
+
+
+        horizontalLayout->addWidget(widget_5);
+
+        widget_4 = new QWidget(widget_2);
+        widget_4->setObjectName(QString::fromUtf8("widget_4"));
+        verticalLayout_2 = new QVBoxLayout(widget_4);
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        verticalLayout_2->setContentsMargins(0, 0, 0, 0);
+        btnShow = new QPushButton(widget_4);
+        btnShow->setObjectName(QString::fromUtf8("btnShow"));
+
+        verticalLayout_2->addWidget(btnShow);
+
+
+        horizontalLayout->addWidget(widget_4);
+
+
+        verticalLayout->addWidget(widget_2);
+
+        widget = new QWidget(centralwidget);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        horizontalLayout_3 = new QHBoxLayout(widget);
+        horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
+        horizontalLayout_3->setContentsMargins(0, 0, 0, 0);
+        pteOutput = new QPlainTextEdit(widget);
+        pteOutput->setObjectName(QString::fromUtf8("pteOutput"));
+        pteOutput->setEnabled(true);
+        pteOutput->setReadOnly(true);
+
+        horizontalLayout_3->addWidget(pteOutput);
+
+
+        verticalLayout->addWidget(widget);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 21));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Receipt:", nullptr));
+        btnShow->setText(QCoreApplication::translate("MainWindow", "Show", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
